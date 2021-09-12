e304c0605c6c154a60836d3b6aebeb2b796e5382
Todd Johnson <grandamp@gmail.com>
2021-09-12 18:52:45 -0400

    Initial Commit

--- mtls_custom_auth/app.py
+++ mtls_custom_auth/app.py
@@ -1,64 +1,67 @@
 import json
-import os
-from certvalidator import CertificateValidator, ValidationContext, errors
-import boto3
-from asn1crypto import pem
+import requests
+  
+# define the api-endpoint 
+API_ENDPOINT = "https://vssapi-dev.treasury.gov/vss/rest/"
 
-'''
-Lets load our truststore from s3. Doing this outside of handler function so that this will be loaded only on coldstart.
-If the truststore contents change, you need to update the lambda env var 'TRUSTSTORE_FILE_VERSIONID'
-with the new files versionId. And also update the same in 'API Gateway > Custom domain names > Domain details > Truststore version' and wait till Status becomes Available.
-If APi Gw finds some problem with the truststote, such as could not find complete chain, it will display a warning. The warning details will tell you which cert it has a problem with and the problem. You need to fix the truststore chain till this warning goes away.
-'''
+# define the validation policy
+VSS_POLICY = "1.3.6.1.5.5.7.19.1"
 
-s3_client = boto3.client('s3')
-
-bucket = os.environ.get('TRUSTSTORE_BUCKET')
-key = os.environ.get('TRUSTSTORE_FILENAME')
-versionId = os.environ.get('TRUSTSTORE_FILE_VERSIONID')
-
-download_path = '/tmp/{}'.format(key)
-s3_client.download_file(bucket, key, download_path, ExtraArgs={'VersionId': versionId})
-
-trust_roots = []
-with open(download_path, 'rb') as f:
-    for _, _, der_bytes in pem.unarmor(f.read(), multiple=True):
-        trust_roots.append(der_bytes)
+def pem_to_vsscert(event):
+    # strip PEM tags and newlines for out VSS request
+    pem = event["requestContext"]["authentication"]["clientCert"]["clientCertPem"]
+    pem = pem.replace("-----BEGIN CERTIFICATE-----","")
+    pem = pem.replace("-----END CERTIFICATE-----","")
+    return pem.replace("\n","")
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    ''' Get the client cert from lambda event '''
-    cert = event["requestContext"]["authentication"]["clientCert"]["clientCertPem"].encode()
-
-    '''
-    hard-fail mode, any error in checking revocation is considered a failure. However, if there is no known source of revocation information, it is not considered a failure.
-    This allows us to keep using self signed certs too.
-    '''
-    context = ValidationContext(allow_fetching=True, revocation_mode="hard-fail", trust_roots=trust_roots)
-
-    try:
-      validator = CertificateValidator(cert, validation_context=context)
-      validator.validate_usage(
-        set(['digital_signature', 'key_encipherment']),
-        set(['server_auth', 'client_auth']),
-        True
-      )
-    except Exception as inst:
-      print(type(inst))
-      print(inst.args)
-      print(inst)
-      print("The certificate could not be validated")
-      return {
-        "isAuthorized": "false",
-        "context": {
-          "exception": str(inst.args)
-        }
-      }
+    evtJson = {"ReceivedEvent": event}
+    print(json.dumps(evtJson))
+    cert = pem_to_vsscert(event)
+    # data to be sent to api
+    data = {"validationPolicy":VSS_POLICY,
+            "wantBackList":[],
+            "x509CertificateList":[{
+                "x509Certificate":cert
+                }]
+            }
+    # sending post request and saving response as response object
+    response = requests.post(url = API_ENDPOINT, json=data)
+    # parse the VSS response
+    resJson = response.json()
+    # parse the individaul resultByCertificate
+    certResult = resJson["validationResult"]["resultsByCertificateList"][0]
+    validationResultToken = certResult["resultByCertificate"]["validationResultToken"]
+    # parse subject from the VSS response to add as principalId to the response policy
+    principalId = certResult["resultByCertificate"]["x509SubjectName"]
+    # if the validationResultToken is "FAIL", then respond with access denial, and provide the VSS invalidityReasonText
+    if validationResultToken == "FAIL":
+        invalidityReasonText = certResult["resultByCertificate"]["validationFailureData"]["invalidityReasonList"][0]["invalidityReasonText"]
+        return {
+            "principalId": principalId,
+            "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Deny",
+                "Resource": "*"
+            }
+            ]},
+            "context": {"exception":invalidityReasonText}
+        } 
+    # otherwise, grant access
     else:
-      print("The certificate is ok")
-      return {
-        "isAuthorized": "true",
-        "context": {
-          "exception": None
+        return {
+            "principalId": principalId,
+            "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+            {
+                "Action": "execute-api:Invoke",
+                "Effect": "Allow",
+                "Resource": "*"
+            }
+            ]},
+            "context": {"exception":None}
         }
-      }
