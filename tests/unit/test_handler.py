eec953146668be12fa3812c807d4794d271ba444
Todd Johnson <grandamp@users.noreply.github.com>
2021-09-14 22:57:44 -0400

    WIP: Updating test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -10,7 +10,7 @@ from mtls_custom_auth import app
 def apigw_event():
     """ Loads API GW Event"""
     tests_root = os.path.dirname(__file__)
-    with open(os.path.join(tests_root,'../../events/event_self_signed.json'), 'rb') as f:
+    with open(os.path.join(tests_root,'../../events_and_trust_store/event_dev_ocio_ca.json'), 'rb') as f:
         event = json.load(f)
 
     return event
@@ -21,6 +21,7 @@ def test_lambda_handler(apigw_event, mocker):
     ret = app.lambda_handler(apigw_event, "")
     print("Received ret: " + json.dumps(ret, indent=2))
 
+    # Probably need to refactor, due to "EnableSimpleResponses: false"
     assert "isAuthorized" in ret
     assert ret["isAuthorized"] == "true"
     assert ret["context"]["exception"] == None
